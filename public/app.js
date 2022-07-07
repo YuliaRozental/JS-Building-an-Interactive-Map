bef1a98b589b2ff6de1cc4d712ce93eefdc2f4fc
Yulia <yuyarozental@gmail.com>
2022-07-07 12:25:57 -0400

    map

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ const myMap = L.map('map', {
 // Add OpenStreetMap tiles:
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(myMap);
 
 // Create and add a geolocation marker:
@@ -16,7 +16,7 @@ marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 // Draw the 2nd Arrondissement      
-var latings = [                                    
+const latings = [                                    
 [48.863368120198004, 2.3509079846928516],
 [48.86933262048345, 2.3542531602919805],
 [48.87199261164275, 2.3400569901592183],
@@ -24,14 +24,29 @@ var latings = [
 [48.86834104280146, 2.330308418109664]
 ];
 
-var polygon = L.polygon(latings, { color: 'red'}).addTo(myMap);
+const polygon = L.polygon(latings, {
+     color: 'blue',
+    }).addTo(myMap);
 
 
+var redPin = L.icon({
+    iconUrl: './assets/red-pin.png',
+    iconSize: [38, 38],
+    iconAnchor: [19, 38],
+    popupAnchor:  [0, -38]
+    });
 
-// create red pin marker
 
+// Metro station markers:
+const rS = L.marker([48.866200610611926, 2.352236247419453], {icon: redPin}).bindPopup('Réaumur-Sébastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196], {icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739], {icon: redPin}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167], {icon: redPin}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543], {icon: redPin}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714], {icon: redPin}).bindPopup('Grands Boulevards')
 
-// metro station markers
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
+
 
 
 
