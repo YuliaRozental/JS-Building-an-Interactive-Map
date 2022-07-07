b6eb4cd0c416928f60762f9fa88a31ceb65bfb09
Yulia <yuyarozental@gmail.com>
2022-07-06 20:32:25 -0400

    practice

--- public/app.js
+++ public/app.js
@@ -1,13 +1,31 @@
 // create map
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
+
+// Add OpenStreetMap tiles:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(myMap);
+
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
-// add openstreetmap tiles
+// Draw the 2nd Arrondissement      
+var latings = [                                    
+[48.863368120198004, 2.3509079846928516],
+[48.86933262048345, 2.3542531602919805],
+[48.87199261164275, 2.3400569901592183],
+[48.86993336274516, 2.3280142476578813], 
+[48.86834104280146, 2.330308418109664]
+];
 
+var polygon = L.polygon(latings, { color: 'red'}).addTo(myMap);
 
-// create and main add geolocation marker
-
-
-// draw the 2nd arrondissement
 
 
 // create red pin marker
